eda948aa09493f814217f288910a1316547ac131
mgutierrez1477 <mgutierrez1477@gmail.com>
2026-02-09 20:07:59 -0600

    fix installed ap cloudinary

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -24,13 +24,13 @@ ALLOWED_HOSTS = [
 
 # Application definition
 INSTALLED_APPS = [
+    'cloudinary_storage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles', # Debe ir antes de cloudinary_storage
-    'cloudinary_storage',
     'cloudinary',
     'rest_framework',
     'corsheaders',
