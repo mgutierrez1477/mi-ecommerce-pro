a758149b0470633fa68f51b1c5427dd7023a4636
mgutierrez1477 <mgutierrez1477@gmail.com>
2026-02-10 18:42:20 -0600

    Fix: settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -105,13 +105,13 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
 # 2. Compatibilidad: La librería cloudinary-storage falla si no ve estas dos variables
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
